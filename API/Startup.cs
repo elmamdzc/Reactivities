a49f53e7fb5a4e0d31f12af558b88298baa7923d
Elma Midzic <elma.midzic@outlook.com>
2019-12-17 16:06:08 +0100

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
+using MediatR;
+using Application.Activities;
 
 namespace API
 {
@@ -36,6 +32,7 @@ namespace API
                 {
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");                });
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
           //  services.AddControllers();
         }
@@ -63,7 +60,6 @@ namespace API
             {
                 endpoints.MapControllers();
             });
-
         
         
         }
